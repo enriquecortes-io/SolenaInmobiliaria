ba96f6f30bb0763a65a467778be3138d84bf769f
Kike Cortés <enriquecortesgomez@gmail.com>
2026-05-14 22:33:35 +0000

    fix: video objectPosition center top for mobile

--- components/Home/SkyHeader.tsx
+++ components/Home/SkyHeader.tsx
@@ -84,7 +84,7 @@ export default function SkyHeader({ locale }: Props) {
           position:"absolute", inset:0,
           width:"100%", height:"100%",
           objectFit:"cover",
-          objectPosition:"center center",
+          objectPosition:"center top",
           zIndex:0,
         }}
       />
