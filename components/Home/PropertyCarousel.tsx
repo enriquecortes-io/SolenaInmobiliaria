d749ae1f1bd7e3ee83e05d8bfdd80dd1850b484a
realtymarbella-tech <realty.marbella@gmail.com>
2026-05-23 08:36:24 +0000

    fix: add missing convertGDriveUrl import in PropertyCarousel

--- components/Home/PropertyCarousel.tsx
+++ components/Home/PropertyCarousel.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { convertGDriveUrl } from "@/lib/gdrive";
 import { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import CarouselScrollIndicator from "./CarouselScrollIndicator";
