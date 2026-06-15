35a39772df5b767cdb7239323542c4d5e557e2ad
Kike Cortés <enriquecortesgomez@gmail.com>
2026-06-15 22:39:23 +0000

    fix: desactivar maintenance mode Solena

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const MAINTENANCE = true; // Cambia a false para desactivar
+const MAINTENANCE = false; // Cambia a false para desactivar
 
 export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
