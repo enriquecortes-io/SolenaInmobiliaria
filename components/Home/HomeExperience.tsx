f40e2a61bd0e0162eb3b0b5ebf63145530d4f7cf
Kike Cortés <enriquecortesgomez@gmail.com>
2026-06-16 14:58:35 +0000

    fix: añadir HeroHeader al JSX de HomeExperience

--- components/Home/HomeExperience.tsx
+++ components/Home/HomeExperience.tsx
@@ -11,7 +11,10 @@ interface Props { locale: string; }
 export default function HomeExperience({ locale }: Props) {
   return (
     <div style={{ width:"100%", background:"#FAFAF7" }}>
-      <MasonrySection locale={locale} />
+      <HeroHeader locale={locale} />
+      <div id="masonry-section">
+        <MasonrySection locale={locale} />
+      </div>
       <Captacion locale={locale} />
       <Footer locale={locale} />
     </div>
