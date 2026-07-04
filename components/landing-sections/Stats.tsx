fb5fe7142fc454d053ab0fb9a4bea1c2964efc52
Kike Cortés <enriquecortesgomez@gmail.com>
2026-07-04 23:34:30 +0000

    chore: eliminar estructura [locale] redundante

---
+++ components/landing-sections/Stats.tsx
@@ -0,0 +1,7 @@
+export default function Stats({ data }: { data: any[] }) {
+  return (
+    <section style={{ background: '#6B3F2A', padding: '40px' }}>
+      {/* Tu código de stats aquí */}
+    </section>
+  );
+}
